7cad02441d2c93bba4965033dc5fd415a88689c3
carlossantosdf <carlos.santosdf@gmail.com>
2024-08-21 16:11:46 -0300

    fix chave in env/main and added shell command in production playbook

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -2,7 +2,7 @@ module "aws-dev" {
   source = "../../infra"
   instancia = "t2.micro"
   regiao_aws = "us-west-2"
-  chave = "IaC-DEV"
+  chave = "IaC-dev"
 }
 output "IP" {
   value = module.aws-dev.IP_publico
