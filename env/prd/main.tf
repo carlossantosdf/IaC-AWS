25f513242073c8219b2cc0d8aa33de554d2f6d38
carlossantosdf <carlos.santosdf@gmail.com>
2024-08-22 16:52:03 -0300

    Modified module to aws-prd

--- env/prd/main.tf
+++ env/prd/main.tf
@@ -1,7 +1,11 @@
-module "aws-dev" {
+module "aws-prd" {
   source = "../../infra"
   instancia = "t2.micro"
   regiao_aws = "us-west-2"
   chave = "IaC-prod"
   grupo = "production"
+}
+
+output "IP" {
+  value = module.aws-prd.IP_publico
 }
